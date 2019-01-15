5bb48be3a8a9e37a32e4b3214b83837bc30d5aef
Virginie Monod <vmonod7@gmail.com>
2019-01-15 13:31:35 +0100

    First Commit

---
+++ index-instadogs.php
@@ -0,0 +1,25 @@
+<!--
+
+ +-+-+-+-+-+-+-+-+-+
+ |I|n|s|t|a|D|o|g|s|
+ +-+-+-+-+-+-+-+-+-+
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="bootstrap-4.2.1-dist/css/bootstrap.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>InstaDogs - Accueil</title>
+</head>
+
+<body>
+
+<p>Salut, moi c'est Moomoo, je suis un maskass. J'ai pas de nez, mais je suis tout moelleux et tout gentil. Comme ma maman. Wheoh.</p>
+    
+</body>
+</html>
