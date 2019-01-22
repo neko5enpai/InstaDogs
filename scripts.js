41c00de106669a7e5d664a8cb2401e65f916368f
Ider Anah <mcsavumingo@gmail.com>
2019-01-22 16:15:47 +0100

    carousel  a styler 2

--- scripts.js
+++ scripts.js
@@ -1,22 +1,17 @@
 $(document).on('ready', function () {
-
     $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    centerMode:true,
-    asNavFor: '.slider-nav',
-    autoplay: true
-    });
-
-    $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: false,
-    centerMode:true,
-    focusOnSelect: true
-
-    });
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+      });         
 });
