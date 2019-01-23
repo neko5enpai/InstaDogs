131a9df847b039ccf03d738636b60ea8ebf45672
Virginie Monod <vmonod7@gmail.com>
2019-01-23 14:29:53 +0100

    profil mis à jour

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 
   
-  $(".albery-container").albery({
-    speed: 200, // default: 200
-    imgWidth: 1024, // default: 600
-    paginationBorder: 7,
-    paginationItemWidth: 100
-  });
+ $(".albery-container").albery({
+  speed: 200, // default: 200
+  imgWidth: 1024, // default: 600
+  paginationBorder: 7,
+  paginationItemWidth: 100
+});
+
