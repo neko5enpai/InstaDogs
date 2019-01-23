6a1340cbe55ef6e58f28a9f4750653c255eac933
Ider Anah <mcsavumingo@gmail.com>
2019-01-23 14:31:33 +0100

    dogsprofile a faire

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 
-  
   $(".albery-container").albery({
     speed: 200, // default: 200
-    imgWidth: 1024, // default: 600
+    imgWidth: 500, // default: 600
     paginationBorder: 7,
     paginationItemWidth: 100
   });
