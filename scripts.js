9348b5e26a371ae159f3b0696b5053d7136be61f
Ider Anah <mcsavumingo@gmail.com>
2019-01-29 11:37:30 +0100

    dog-profile, profile scripts styles

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 
-  $(".albery-container").albery({
-    speed: 200, // default: 200
-    imgWidth: 500, // default: 600
-    paginationBorder: 7,
-    paginationItemWidth: 140
-  });
+$(".albery-container").albery({
+  speed: 200, // default: 200
+  imgWidth: 500, // default: 600
+  paginationBorder: 7,
+  paginationItemWidth: 140
+});
+
+
+$("")
 
