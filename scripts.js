e903ed534666c0d8a8a1dbd7c0e438998c2a05be
Ider Anah <mcsavumingo@gmail.com>
2019-01-29 13:51:24 +0100

    delete dog ok

--- scripts.js
+++ scripts.js
@@ -1,16 +1,21 @@
-$(function() {
-  $( ".fas" ).click(function() {
-    $( ".dogs" ).remove();
+/* $(function() {
+  $( ".fas" ).click(function(e) {
+    e.parent.remove();
   });
 });
+ */
 
+$(".delete_dog").click(function(event) {
+  event.preventDefault();
+  $(this).parent('.dogs').remove();
+});
 
-$(".albery-container").albery({
+/* $(".albery-container").albery({
   speed: 200, // default: 200
   imgWidth: 500, // default: 600
   paginationBorder: 7,
   paginationItemWidth: 140
-});
+}); */
 
 
 
