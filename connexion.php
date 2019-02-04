c4cba79d2c2e40b556a99aa62d5e01f079559faf
Virginie Monod <vmonod7@gmail.com>
2019-02-04 10:54:15 +0100

    message effacé

--- connexion.php
+++ connexion.php
@@ -3,11 +3,6 @@ password: Inst@D0g
 
 <?php
 
-/* Cher Rodens,
-Je suis désolée, j'ai rien foutu de la matinée... et je ne suis pas là (01/02/2019)
-Mais si jamais je ne suis pas là de la journée, tu peux me contacter depuis WhatsApp
-Bisous moelleux de mes petits maskass. 
 
-Virginie */
 
 ?>
